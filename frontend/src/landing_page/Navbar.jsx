cd6ba103a5547cf560f9c0ea54c3052b842666aa
Pawan Yadav <hivepawan@gmail.com>
2026-06-06 18:05:55 +0530

    feat: complete homepage with navbar, footer, and OpenAccount components

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 
 function Navbar() {
-  return ( 
-    <h1>Navbar</h1>
+  return (
+    <nav class="navbar navbar-expand-lg bglight border-bottom">
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          <img src="media/images/logo.svg" alt="logo" style={{ width: "25%" }} />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav  mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-muted" aria-current="page" href="#">Signup</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active text-muted" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active text-muted" href="#">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active text-muted" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active text-muted" href="#">Support</a>
+              </li>
+              {/* hamburger menu */}
+              {/* <li class="nav-item">
+                <a class="nav-link active text-muted" href="#"><i class="fa-solid fa-bars"></i></a>
+              </li> */}
+
+            </ul>
+
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 }
 
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Hero() {
   return ( 
-    <div className='container p-5 mb-5'>
+    <div className='container p-5'>
       <div className='row text-center'>
-        <img src="media/images/homeHero.png" alt="Home Hero Image"  className='mb-5'/>
-        <h1 className='mt-5'>Invest in everything</h1>
+        <img src="media/images/homeHero.png" alt="Home Hero Image"  className='mb-5 mx-auto' style={{width: "65%"}}/>
+        <h2 className='mt-4'>Invest in everything</h2>
         <p>
           Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
         </p>
-        <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: "20%", margin: "0 auto"}}>Sign up for free</button>
+        <button className='p-2 btn btn-primary fs-5' style={{width: "20%", margin: "0 auto"}}>Sign up for free</button>
       </div>
     </div>
   );
